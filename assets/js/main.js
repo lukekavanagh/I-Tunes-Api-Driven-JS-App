ebacfff4bfe6cb4f7e16e654832f824e190ad58e
lukekavanagh <lukekavanagh1@yahoo.co.uk>
2015-11-15 20:37:28 +1300

    fixed error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,41 @@
-var Gimme = {
-	$content: $('.content'),
-	$form: $('form'),
-	userInput: '',
-	userInputisValid: false,
-	appId: '',
+var Gimmie = {
+    $content: $('.content'),
+    $form: $('form'),
+    userInput: '',
+    userInputIsValid: false,
+    appId: '',
+
+    toggleLoading: function(){
+        // Toggle loading indicator
+        this.$content.toggleClass('content--loading');
+
+        // Toggle the submit button so we don't get double submissions
+        // http://stackoverflow.com/questions/4702000/toggle-input-disabled-attribute-using-jquery
+        this.$form.find('button').prop('disabled', function(i, v) { return !v; });
+    },
+
+    throwError: function(header, body){
+        // Remove animation class
+        this.$content.removeClass('content--error-pop');
+
+        // Trigger reflow
+        // https://css-tricks.com/restart-css-animation/
+        this.$content[0].offsetWidth = this.$content[0].offsetWidth;
+
+        // Add classes and content
+        this.$content
+            .html('<p><strong>' + header + '</strong> ' + body + '</p>')
+            .addClass('content--error content--error-pop');
+
+        this.toggleLoading();
+    },
+
     validate: function() {
-        //use regex to test if input is valid. It's valid if :
-        // 1. It begins with 'http://itunes'
-        // 2. It has '/id' followed by digits in the string somewhere
-        var regUrl = /^(http|https):\/\/itunes/;
-        var regId = /\/id()(\d+)/i;
+        // Use regex to test if input is valid. It's valid if:
+        //  1. It begins with 'http://itunes'
+        //  2. It has '/id' followed by digits in the string somewhere
+        var regUrl = /^(http|https):\/\/itunes/,
+            regId = /\/id(\d+)/i;
         if ( regUrl.test(this.userInput) && regId.test(this.userInput) ) {
             this.userInputIsValid = true;
             var id = regId.exec(this.userInput);
@@ -18,35 +44,29 @@ var Gimme = {
             this.userInputIsValid = false;
             this.appId = '';
         }
-    }
-
-throwError: function(header, text){
-    //Remove animation class
-    this.$content.removeClass('content--error-pop');
-
-    // Trigger reflow
-    //https://csstricks.com/restart-css-animation/
-    this.$content[0].offsetWidth = this.$content[0].offsetWidth;
-
-    this.$content
-        .html('<p><strong>' + header + '</strong> ' + text + '</p>' )
-        .addClass('content--error');
-
-        this.toggleLoading();
-    }
-}
-
-    toggleLoading: function(){
-        // Toggle loading indicator
-        this.$content.toggleClass('content--loading');
-         
-        // Toggle the submit button so we don't get double submissions
-        // http://stackoverflow.com/questions/4702000/toggle-input-disabled-attribute-using-jquery
-        this.$form.find('button').prop('disabled', function(i, v) { return !v; });
     },
-}
 
+    render: function(response){
+        var icon = new Image();
+        icon.src = response.artworkUrl512;
+        icon.onload = function() {
+            Gimmie.$content
+                .html(this)
+                .append('<p><strong>' + response.trackName + '</strong> Actual icon dimensions: ' + this.naturalWidth + '×' + this.naturalHeight + '</p>')
+                .removeClass('content--error');
+            Gimmie.toggleLoading();
 
+            // If it's an iOS icon, load the mask too
+            if(response.kind != 'mac-software') {
+                var mask = new Image();
+                mask.src = 'assets/img/icon-mask.png';
+                mask.onload = function() {
+                    Gimmie.$content.prepend(this);
+                }
+            }
+        }
+    }
+};
 
 $(document).ready(function(){
     Gimmie.$form.on('submit', function(e){
@@ -54,14 +74,18 @@ $(document).ready(function(){
         Gimmie.toggleLoading(); 
         Gimme.userInput = $(this).find('input').val();
         Gimme.validate();
-        if ( Gimmie.userInputIsValid ) {
-        //make API request
-        } else {
-            Gimme.throwError(
-                'Invalid Link'
-                'You must submit a standard ITunes Store Link with an ID, i.e <br> <a href="https://itunes.apple.com/us/app/twitter/id333903271?mt=8">https://itunes.apple.com/us/app/twitter/<em>id333903271</em>?mt=8</a>'
-                );
-        // throw an error
-    	}
     });
 });
+
+
+//         if ( Gimmie.userInputIsValid ) {
+//         //make API request
+//         } else {
+//             Gimme.throwError(
+//                 'Invalid Link'
+//                 'You must submit a standard ITunes Store Link with an ID, i.e <br> <a href="https://itunes.apple.com/us/app/twitter/id333903271?mt=8">https://itunes.apple.com/us/app/twitter/<em>id333903271</em>?mt=8</a>'
+//                 );
+//         // throw an error
+//     	}
+//     });
+// });
